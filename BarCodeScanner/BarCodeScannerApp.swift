cccafa2c7b85338bf6b1758d3c8fad32d4d545c5
Lucas <107668539+lfomar1@users.noreply.github.com>
2024-02-02 11:51:19 -0300

    Initial Commit

---
+++ BarCodeScanner/BarCodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarCodeScannerApp.swift
+//  BarCodeScanner
+//
+//  Created by Lucas Omar on 2024-02-02.
+//
+
+import SwiftUI
+
+@main
+struct BarCodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
